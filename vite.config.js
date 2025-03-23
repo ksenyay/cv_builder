4493f0876cf0496503f16020e3fcb50827cf794d
ksenyay <ksenyausachova@gmail.com>
2025-03-23 20:57:04 +0200

    Deployment commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,5 @@ import react from "@vitejs/plugin-react";
 
 export default {
   plugins: [react()],
-  base: "/cv_builder/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
+  base: "/cv_builder",
 };
